6022363da7fdaeaa828d3bc4c52c7ad54f58686f
Manish-0003 <manish.sharma9314@gmail.com>
2025-12-03 20:11:41 +0530

    cart css modify

--- my-app/src/components/Pages/Gallery/Gallery.jsx
+++ my-app/src/components/Pages/Gallery/Gallery.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React from "react";
 import "./Gallery.css";
+import Navbar from "../../Navbar/Navbar";
+import Footer from "../../Footer/Footer";
+
+import img1 from "../../../assets/chef.jpeg";
+import img2 from "../../../assets/about2.jpeg";
+import img3 from "../../../assets/about.jpeg";
+import img4 from "../../../assets/burger.jpeg";
+import img5 from "../../../assets/masala_dosa.jpeg";
+
+import img6 from "../../../assets/dishes.jpeg";
+
+const images = [img1, img2, img3, img4, img5, img6];
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
-  )
-}
+    <>
+      <Navbar />
+      <section className="gallery-section">
+        <h1>Our Gallery</h1>
+        <p>Take a glimpse of our delicious dishes and cozy ambiance.</p>
+
+        <div className="gallery-grid">
+          {images.map((img, index) => (
+            <div key={index} className="gallery-item">
+              <img src={img} alt={`dish-${index}`} />
+            </div>
+          ))}
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
 
 export default Gallery;
